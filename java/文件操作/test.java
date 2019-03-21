e8e90071844ad4e429078ef55e24c26b6b304c57
Raymond-du <36786348+Raymond-du@users.noreply.github.com>
2019-03-21 12:34:07 +0800

    Rename java/solar/test.java to java/文件操作/test.java

--- java/文件操作/test.java
+++ java/文件操作/test.java
@@ -15,7 +15,7 @@ public class test {
 	}
     }
     public static void main(String[] args) throws IOException {
-	File f=new File("C:\\Users\\26368\\Documents\\Visual Studio 2013\\Դ��\\tip\\sysopt\\bin\\debug");
+	File f=new File("C:\\Users\\26368\\Documents\\Visual Studio 2013\\Ô´Âë\\tip\\sysopt\\bin\\debug");
 	File fList[]=f.listFiles(new FilenameFilter() {
 	    
 	    @Override
